bc67999998a7c60493c09d3b306b696b5c6ae31b
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-08-21 18:20:26 -0500

    Complete Hover effect on grid squares changing to purple on mouse enter

--- etch.js
+++ etch.js
@@ -5,18 +5,43 @@ const square = document.createElement('div');
 gridrow.classList.add('gridrow');
 square.classList.add('square');
 
-function changeBackground() {
-    square.classList.add("painted");
-    console.log(square);
+//temp globals
+const ROWS = 16;
+const COLS = 16;
+const TOTAL = ROWS * COLS;
+
+//populate row
+function populateRow(columns, row) {
+    for(let j = 0; j < columns; j++) {
+        const temps = square.cloneNode(true);
+        let id = "cell" + row.id + ":" + j;
+        //hover(temps, changeBackground, fadeBack);
+        temps.setAttribute('id', id);
+        temps.addEventListener("mouseenter", (event) => {
+            // highlight the mouseenter target
+            event.target.style.backgroundColor = "purple";
+            console.log(event.target.style.color);
+          
+            // reset the color after a short delay
+            setTimeout(() => {
+              event.target.style.backgroundColor = "";
+            }, 500);
+          }, false);
+        row.appendChild(temps);
+    }
 }
 
-//adding mousover event listener to change pixels
-square.addEventListener("mouseover", changeBackground());
+//populate grid
+function populateGrid(columns, rows) {
+    for(let i = 0; i < rows; i++) {
+        const row = gridrow.cloneNode(true);
+        let id = i;
+        row.setAttribute('id', id);
+        populateRow(columns, row);
+        grid.appendChild(row);
+    }
+}
+populateGrid(COLS, ROWS);
 
-for(let j = 0; j < 16; j++) {
-    gridrow.appendChild(square.cloneNode(true));
-}
-for(let i = 0; i < 16; i++) {
-    grid.appendChild(gridrow.cloneNode(true));
-}
+
 
