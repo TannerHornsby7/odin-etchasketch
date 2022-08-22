8db768b052c1edbefd98738313bd8d8d9d3f9b4d
Tanner Hornsby <tannerhornsby7@gmail.com>
2022-08-21 19:12:46 -0500

    Change error message to alert message so user knows to change input

--- etch.js
+++ etch.js
@@ -58,7 +58,7 @@ button.addEventListener('click', (e) => {
     let str_length = prompt("Please input your desired canvas size(less than 100px):");
     let input_length = parseInt(str_length);
     if(input_length > 100) {
-        return Error("Please select a length smaller than 100px");
+        return alert("Please select a length smaller than 100px");
     }
     clearGrid();
     populateGrid(input_length, input_length);
